cbdea8bafbf103aa9360d332eb8790479451d8d0
Kumar Sahil <2270168@kiit.ac.in>
2024-02-12 16:36:59 +0530

    mongodb 2

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,30 @@ router.get("/", function (req, res) {
   res.render("index");
 });
 
-//creating in mongodb
-router.get("/create", async function (req, res) {
+//creating db and user in mongodb
+/* router.get("/create", async function (req, res) {
   const createduser = await sahilModel.create({
     username: "Kumar",
     name: "sahil",
     age: 25,
+  }); */
+//Now this is asynchronous hence "createduser" will run first and then sahil model will be created, this is wrong
+//if you want to run sahilmodel first then put "await" ifront of sahilmodel
+//now "await" is not used alone, "async" is used with it in the parent function
+//res.send(createduser);
+
+//finding or reading user
+router.get("/allusers", async function (req, res) {
+  let all = await sahilModel.find();
+  res.send(all);
+});
+
+//deleting user
+router.get("/delete", async function (req, res) {
+  let deleteduser = await sahilModel.findOneAndDelete({
+    username: "Kumar",
   });
-  //Now this is asynchronous hence "ban gya" will run first and then sahil model will be created, this is wrong
-  //if you want to run sahilmodel first then put "await" ifront of sahilmodel
-  //now "await" is not used alone, "async" is used with it in the parent function
-  res.send(createduser);
+  res.send(deleteduser);
 });
 
 module.exports = router;
